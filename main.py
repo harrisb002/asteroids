0e5de3f89c8d0136bd32b842a94703a730966c18
harrisb002 <harrisbe@sonoma.edu>
2025-02-16 22:17:05 -0800

    adding asteroids

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    # Create groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    # Assign groups to classes
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+    # Create game objects
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
     dt = 0
 
     while True:
@@ -15,13 +31,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        # Update all objects
+        updatable.update(dt)
 
+        # Draw all objects
         screen.fill("black")
-        player.draw(screen)
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip()
 
-        # limit the framerate to 60 FPS
+        # Limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
--- player.py
+++ player.py
@@ -29,3 +29,7 @@ class Player(CircleShape):
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        direction = pygame.Vector2(0, -1).rotate(self.rotation)  
+        self.position += direction * PLAYER_SPEED * dt          
